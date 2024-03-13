082a2e1545857b74046a4d78992b0a5d369763d0
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2024-03-13 18:24:33 +0530

    adding

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <img
+        src="saipraveen.jpeg" 
+        alt="My Profile Pic"
+        className="profile-pic"
+      />
+      <h1 className="message">Hello My Self SAI SIVA MANI</h1>
+      <a href='https://drive.google.com/file/d/1H6hzRfrjK4WNQxQcW_udJl9B8qvXgAhr/view?usp=sharing' target='new' className="button">Check Out My Resume</a>
+       
+       <h2>My Skills</h2>
+    </div>
+  );
+}
+
+export default App;
